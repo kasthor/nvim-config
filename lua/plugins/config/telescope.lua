cc28d209964e64efe8167dcd2c7c18c86873e4be
Giancarlo Palavicini <kasthor@gmail.com>
2023-04-01 12:52:46 -0600

    fix: config files location

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 
 local config_files = function()
-  return builtin.find_files({ cwd = "~/.dotfiles/nvim/" })
+  return builtin.find_files({ cwd = "~/.config/nvim/" })
 end
 
 vim.keymap.set('n', '<Leader>o', builtin.find_files, {})
