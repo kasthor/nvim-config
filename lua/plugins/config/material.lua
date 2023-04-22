0139603547edd3577e59a46fdd0232e7d01e3df7
Giancarlo Palavicini <kasthor@gmail.com>
2023-04-22 10:38:55 -0600

    feat: line numbers color

--- lua/plugins/config/material.lua
+++ lua/plugins/config/material.lua
@@ -1,35 +1,11 @@
 require('material').setup({
-  --contrast = true, -- Enable contrast for sidebars, floating windows and popup menus like Nvim-Tree
   borders = true, -- Enable borders between verticaly split windows
-  --popup_menu = 'dark', -- Popup menu stylke ( can be: 'dark', 'light', 'colorful' or 'stealth' )
-
-  --italics = {
-  --    comments = true, -- Enable italic comments
-  --    keywords = false, -- Enable italic keywords
-  --    functions = false, -- Enable italic functions
-  --    strings = false, -- Enable italic strings
-  --    variables = false, -- Enable italic variables
-  --},
-
-  --contrast_windows = { -- Specify which windows get the contrasted (darker) background
-  --    'toggleterm',
-  --    'terminal', -- Darker terminal background
-  --    'packer', -- Darker packer background
-  --    'qf', -- Darker qf list background
-  --},
-
-  --text_contrast = {
-  --    lighter = false, -- Enable higher contrast text for lighter style
-  --    darker = false, -- Enable higher contrast text for darker style
-  --},
-
-  --disable = {
-  --    background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
-  --    term_colors = false, -- Prevent the theme from setting terminal colors
-  --    eob_lines = false, -- Hide the end-of-buffer lines
-  --},
-
-  --custom_highlights = {}, -- Overwrite highlights with your own
+  -- for available colors:
+  -- https://github.com/marko-cerovac/material.nvim/blob/main/lua/material/colors/init.lua
+  --
+  custom_colors = function(colors)
+    colors.editor.line_numbers = colors.main.darkpurple
+  end
 })
 
 vim.g.material_style = 'palenight'
