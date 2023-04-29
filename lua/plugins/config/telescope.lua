a6dfdc95932b131f55a6fdfcb1a7f7db0de451e8
Giancarlo Palavicini <kasthor@gmail.com>
2023-04-29 08:59:13 -0600

    feat: additional keymaps

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -7,3 +7,4 @@ end
 vim.keymap.set('n', '<Leader>o', builtin.find_files, {})
 vim.keymap.set('n', '<Leader>oc', config_files, {})
 vim.keymap.set('n', '<Leader>og', builtin.live_grep, {})
+vim.keymap.set('n', '<Leader>ob', builtin.buffers, {})
