db5c388492a7d5ea88f70e4f2311023a4c1a097a
Giancarlo Palavicini <kasthor@gmail.com>
2023-06-01 05:55:51 -0600

    refactor: formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,16 @@
 return {
   -- [[ theming ]] --
   'kyazdani42/nvim-web-devicons',
+
   -- [[ surround ]] --
   'tpope/vim-surround',
+
   -- [[ comments ]] --
   { 'numToStr/Comment.nvim', config = true },
-  -- [ git ] =-
-  'tpope/vim-fugitive'
 
+  -- [ git ] =-
+  'tpope/vim-fugitive',
+
+  -- [[ test runner ]] --
+  'kasthor/nvim-test-runner'
 }
