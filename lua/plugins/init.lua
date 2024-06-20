552307266dab624ab258918e963abdfaa96d3b1e
Giancarlo Palavicini <kasthor@gmail.com>
2024-06-20 15:18:07 -0600

    feat: change start screen

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,7 +29,7 @@ return {
     'goolord/alpha-nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require 'alpha'.setup(require 'alpha.themes.startify'.config)
+      require 'alpha'.setup(require 'alpha.themes.theta'.config)
     end
   },
   -- {
