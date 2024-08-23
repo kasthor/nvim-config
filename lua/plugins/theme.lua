27686715a4b347aa1ba4d4edafd51b3f12a629e0
Giancarlo Palavicini <kasthor@gmail.com>
2024-08-23 09:28:03 -0600

    Catpucchin

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+return {
+  "catppuccin/nvim",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      transparent_background = false
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end
+}
