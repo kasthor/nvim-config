9ce4356e202ff6d507b4694bc76cb7b5ce44e696
Giancarlo Palavicini <kasthor@gmail.com>
2024-12-06 13:54:16 -0600

    fix: neo-tree configuration

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  branch = "v2.x",
+  branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     -- "nvim-tree/nvim-web-devicons",
@@ -15,7 +15,7 @@ return {
         winbar = true
       },
       filesystem = {
-        follow_current_file = true,
+        follow_current_file = { enabled = true },
       },
       window = {
         mappings = {
